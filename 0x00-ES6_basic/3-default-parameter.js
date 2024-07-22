8c2e9e1836a2b8bb83a33591b21d736f3def12ec
Gbolahan Akande <geakande@gmail.com>
2024-07-22 17:17:23 +0000

    Rewriting a function a function in one line

--- 0x00-ES6_basic/3-default-parameter.js
+++ 0x00-ES6_basic/3-default-parameter.js
@@ -1,1 +1,3 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
 export default (initialNumber, expansion1989 = 89, expansion2019 = 19) => initialNumber + (expansion1989 || 89) + (expansion2019 || 19);
