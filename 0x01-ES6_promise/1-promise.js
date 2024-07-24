7fefc8d45fb96991203df2e4aa3312acf0b1df01
Gbolahan Akande <geakande@gmail.com>
2024-07-24 22:08:32 +0000

    The fake API is not working currently

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -9,7 +9,7 @@ export default function getFullResponseFromAPI(success) {
 			});
 		}
 		else {
-			reject(new Error('The fake API is not working currently');
+			reject(new Error('The fake API is not working currently'));
 		}
 	});
 }
