fec212f3337303a1277d8096964f332ce5151551
Gbolahan Akande <geakande@gmail.com>
2024-07-24 22:25:48 +0000

    The fake API is not working currently

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,19 +1,6 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable */
 export default function handleResponseFromAPI(promise) {
-	return new Promise((resolve, reject) => {
-		if (promise) {
-			resolve({
-				status: 200,
-				body: 'Success'
-			});
-		}
-		else {
-			reject();
-		}
-	})
-}
-export default function handleResponseFromAPI(promise) {
 	return promise
 	.then(() => {
 		return{status: 200, body: 'Success'};
