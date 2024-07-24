07160e4a7e6632eef3b869690dfc04de4574b954
Gbolahan Akande <geakande@gmail.com>
2024-07-24 22:47:47 +0000

    The fake API is not working currently

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -3,12 +3,14 @@
 function handleResponseFromAPI(promise) {
 	return promise
 	.then(() => {
+		console.log('Got a response from the API');
 		return {
 			status: 200,
 			body: 'Success'
 		};
 	})
 	.catch(() => {
+		console.log('Got a response from the API');
 		return new Error();
 	})
 	.finally(() => {
