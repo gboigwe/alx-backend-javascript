ae757f7ea5be296f02a8a634181ae67b80f20a48
Gbolahan Akande <geakande@gmail.com>
2024-07-25 21:33:47 +0000

    Implemented a class named building

---
+++ 0x02-ES6_classes/5-building.js
@@ -0,0 +1,24 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+export default class Building {
+  constructor(sqft) {
+    this.sqft = sqft;
+    
+    if (this.constructor !== Building) {
+      if (typeof this.evacuationWarningMessage !== 'function') {
+        throw new Error('Class extending Building must override evacuationWarningMessage');
+      }
+    }
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+
+  set sqft(value) {
+    if (typeof value !== 'number') {
+      throw new TypeError('sqft must be a number');
+    }
+    this._sqft = value;
+  }
+}
