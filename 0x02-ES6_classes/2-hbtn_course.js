0d8a7a461cc20b7529306072871318c31109e009
Gbolahan Akande <geakande@gmail.com>
2024-07-25 20:22:04 +0000

    Implementing

--- 0x02-ES6_classes/2-hbtn_course.js
+++ 0x02-ES6_classes/2-hbtn_course.js
@@ -1,45 +1,29 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable */
 export default class HolbertonCourse {
-  constructor(name, length, students) {
-    this.name = name;
-    this.length = length;
-    this.students = students;
-  }
+	constructor(name, length, students) {
+		this.name = name;
+		this.length = length;
+		this.students = students;
+	}
+	get name() {
+		return this._name;
+	}
+	set name(value) {
+		this._name = value;
+	}
 
-  // Getter and setter for name
-  get name() {
-    return this._name;
-  }
+	get length() {
+		return this._length;
+	}
+	set length(value) {
+		this._length = value;
+	}
 
-  set name(value) {
-    if (typeof value !== 'string') {
-      throw new TypeError('Name must be a string');
-    }
-    this._name = value;
-  }
-
-  // Getter and setter for length
-  get length() {
-    return this._length;
-  }
-
-  set length(value) {
-    if (typeof value !== 'number') {
-      throw new TypeError('Length must be a number');
-    }
-    this._length = value;
-  }
-
-  // Getter and setter for students
-  get students() {
-    return this._students;
-  }
-
-  set students(value) {
-    if (!Array.isArray(value) || !value.every(student => typeof student === 'string')) {
-      throw new TypeError('Students must be an array of strings');
-    }
-    this._students = value;
-  }
+	get students() {
+		return this._students;
+	}
+	set students(value) {
+		this._students = value;
+	}
 }
