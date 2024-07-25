74685c759d418cf65de6f4e0156e4f809d33a28e
Gbolahan Akande <geakande@gmail.com>
2024-07-25 12:11:38 +0000

    Implementing a class

--- 0x02-ES6_classes/0-classroom.js
+++ 0x02-ES6_classes/0-classroom.js
@@ -1,7 +1,6 @@
 /* eslint-disable no-unused-vars */
 /* eslint-disable */
 export default class ClassRoom {
-	_maxStudentsSize = 0
 	constructor(maxStudentsSize) {
 		this._maxStudentsSize = maxStudentsSize
 	}
