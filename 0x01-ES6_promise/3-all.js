1228a380246b147f1f4fa8c3ea2c21a5578f67f2
Gbolahan Akande <geakande@gmail.com>
2024-07-27 07:27:52 +0000

    uploading photo and creating user

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -3,15 +3,10 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  return Promise.all([uploadPhoto(), createUser()])
-    .then(([photoResponse, userResponse]) => {
-      console.log(
-        photoResponse.body,
-        userResponse.firstName,
-        userResponse.lastName
-      );
+  return Promise
+    .all([uploadPhoto(), createUser()])
+    .then((result) => {
+      console.log(`${result[0].body} ${result[1].firstName} ${result[1].lastName}`);
     })
-    .catch(error => {
-      console.log('Signup system offline');
-    });
+    .catch(() => console.log('Signup system offline'));
 }
