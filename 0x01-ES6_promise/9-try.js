2c2bbeebfccfdc2473ef23beca9001c1b3fdc64f
Gbolahan Akande <geakande@gmail.com>
2024-07-29 15:57:25 +0000

    Handling error message

---
+++ 0x01-ES6_promise/9-try.js
@@ -0,0 +1,16 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+export default function guardrall(mathFunction) {
+	const queue = [];
+	let result;
+	try {
+		result = mathFunction();
+	}
+	catch (err) {
+		result = `${err.message}`;
+	}
+	queue.push(result);
+	queue.push('Guardrall was processed');
+
+	return queue;
+}
