4612cd6e361b2f777d2d765509504a12ee17b51d
Gbolahan Akande <geakande@gmail.com>
2024-07-30 13:20:17 +0000

    Function that returns an array of objects

---
+++ 0x03-ES6_data_manipulation/0-get_list_students.js
@@ -0,0 +1,22 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+export default function getListStudents() {
+        const data = [
+                {
+			id: 1,
+                        firstName: "Guillaume",
+                        location: "San Francisco",
+                },
+                {
+			id: 2,
+                        firstName: "James",
+                        location: "Columbia",
+                },
+                {
+			id: 5,
+                        firstName: "Serena",
+                        location: "San Francisco",
+                }
+        ];
+	return data;
+}
