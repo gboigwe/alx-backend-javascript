91b1a542b867a47dacce9209060fb12d967fd2f7
Gbolahan Akande <geakande@gmail.com>
2024-07-30 16:36:26 +0000

    Function that returns the sum of all the student ids

---
+++ 0x03-ES6_data_manipulation/4-update_grade_by_city.js
@@ -0,0 +1,15 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+let newGrades = {
+  studentId: 31,
+  grade: 78,
+}
+export default function updateStudentGradeByCity(getListStudents, city, newGrades) {
+  return getListStudents.filter(student => student.location === city).map(student => {
+    let gradeS = newGrades.find(grad => grad.studentId === student.Id);
+    return { ...student, gradeS: (gradeS && gradeS.gradeS) || 'N/A' };
+  });
+
+}
