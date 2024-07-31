90b7aa49f2917857d93ead0cf35d823da3b9da11
Gbolahan Akande <geakande@gmail.com>
2024-07-31 00:05:40 +0000

    Writing a weakmap cod

---
+++ 0x03-ES6_data_manipulation/100-weak.js
@@ -0,0 +1,17 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable */
+export const weakMap = new WeakMap();
+
+export function queryAPI(endpoint) {
+  let called = weakMap.get(endpoint) || 0;
+
+  called += 1;
+
+  weakMap.set(endpoint, called);
+
+  if (called >= 5) {
+    throw Error('Endpoint load is high');
+  }
+
+  return called;
+}
